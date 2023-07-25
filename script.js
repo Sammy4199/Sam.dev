8aaab0f2891e121f16a9e7d80384082b63891f71
Sammy4199 <samuelowususamuelowusu41994199@gmail.com>
2023-07-25 00:02:53 +0000

    all new

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 document.ondragstart = function () {
     return false
 };
-
+const img = document.querySelector('.pro__img a img');
 const menu = document.querySelector('.menu');
 const menuBar = document.querySelector('.menubar i');
 menuBar.addEventListener("click", function () {
-    menu.classList.toggle('show')
+    menu.classList.toggle('show');
+    img.style.transform = 'translateY(0%)'
 if (menu.classList.contains('show')) {
-    menuBar.classList.replace('fa-bars', 'fa-times');
+    menuBar.classList.replace('fa-bars-staggered', 'fa-times');
 }else {
-    menuBar.classList.replace('fa-times','fa-bars');
+    menuBar.classList.replace('fa-times','fa-bars-staggered');
 }
 });
+
